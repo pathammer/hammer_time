a9aa444742c69f8da5049b7359e37336c14b23fd
Patrick Hammer <PATRICK.X.HAMMER.-ND@disney.com>
2019-10-17 10:19:21 -0700

    backup system config

--- bin/finder.sh
+++ bin/finder.sh
@@ -11,10 +11,17 @@ arr+=(
     [code]="/home/hammer/code/"
     [tmp]="/tmp"
     [$recent]="$recent"
+    [robotlogs]="/run/user/1000/gvfs/smb-share:server=robotvideos,share=robotlogs//"
+    [project_share]="/run/user/1000/gvfs/smb-share:server=im-cabu-ap004,share=projects/"
+    [datasheets]="/run/user/1000/gvfs/smb-share:server=im-cabu-ap004,share=projects/Advanced Robotics/Kiwi/EE/Data Sheets/"
+    
+    
+    
+
 )
 
 choice=$(printf "%s\n" "${!arr[@]}" | dmenu -l 20)
 echo opening  ${arr[$choice]}
 # Execute choice if dmenu returns ok:
-[ $? = 0 ] && xdg-open ${arr[$choice]}
+[ $? = 0 ] && xdg-open "${arr[$choice]}"
 
