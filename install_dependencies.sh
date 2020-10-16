c54a620e2253539796bbe6957621187d7b3ba4e5
Patrick Hammer <me@patrickhammer.com>
2020-10-16 16:25:00 -0700

    refresh more for 20.04

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,7 +8,7 @@ fi
 apt install i3 zsh fzf emacs htop pv nmap tmux tig ncdu tree ranger xdotool screen imagemagick xclip copyq curl playerctl xautolock scrot gthumb autojump prettyping
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh hammer -s /usr/bin/zsh
-snap install vlc
+snap install vlc ksnip
 snap install --classic slack spotify pycharm-community
 snap install intellij-idea-community --classic --edge
-pip3 install --user py3status ipython jupyter scipy matplotlib numpy pydbus
+pip3 install --user py3status ipython jupyter scipy matplotlib numpy pydbus autotiling
